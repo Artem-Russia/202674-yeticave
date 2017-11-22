d51dcea1f563b2025bb52ac397621a90b90f9bfa
Artem Dorin <gimazetdinov.ar@yandex.ru>
2017-11-22 23:46:56 +0300

    Забыл про отсутпы, поправил

--- index.php
+++ index.php
@@ -108,24 +108,26 @@ $amount_of_lots=count($category);
             <? $current_num=0;
                 while($current_num<$amount_of_lots){
                     $current_num++;?>
-                    <li class="lots__item lot">
-                    <div class="lot__image">
-                    <img src="<?=$lot[$current_num]["url"];?>" width="350" height="260" alt="<?=$lot[$current_num]["name"];?>">
-                    </div>
-                    <div class="lot__info">
-                    <span class="lot__category"><?=$lot[$current_num]["category"];?></span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$lot[$current_num]["name"];?></a></h3>
-                    <div class="lot__state">
-                        <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$lot[$current_num]["price"];?><b class="rub">р</b></span>
-                        </div>
-                        <div class="lot__timer timer">
-                            <?=$lot_time_remaining;?>
-                        </div>
-                    </div>
-                </div>
-            </li>
+                        <li class="lots__item lot">
+                            <div class="lot__image">
+                                <img src="<?=$lot[$current_num]["url"];?>" width="350" height="260" alt="<?=$lot[$current_num]["name"];?>">
+                            </div>
+                            <div class="lot__info">
+                                <span class="lot__category"><?=$lot[$current_num]["category"];?></span>
+                                <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$lot[$current_num]["name"];?></a></h3>
+
+                                <div class="lot__state">
+                                    <div class="lot__rate">
+                                        <span class="lot__amount">Стартовая цена</span>
+                                        <span class="lot__cost"><?=$lot[$current_num]["price"];?><b class="rub">р</b></span>
+                                    </div>
+                                    <div class="lot__timer timer">
+                                        <?=$lot_time_remaining;?>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </li>
             <?};?>
         </ul>
     </section>
