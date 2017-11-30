fdfc32f7f94fb20445c5b9d6b772bc7a0b8c08d8
Artem Dorin <gimazetdinov.ar@yandex.ru>
2017-11-30 13:56:18 +0300

    изменения в функции

--- lot.php
+++ lot.php
@@ -9,20 +9,19 @@ $bets = [
 ];
 function lot_time($arg_1){
     (int)$time_passed=time()-$arg_1;
-    if($time_passed>=86400):
-        $time_passed_date=date("d.m.y.", $arg_1);
+    if($time_passed>=86400){
+        $time_passed_date=date("d.m.y", $arg_1);
         $time_passed_m_h=date("H.i",$arg_1);
-        return "$time_passed_date в $$time_passed_m_h";
-    else:
-        if($time_passed<3600):
-            $time_passed_m=$time_passed/60;
-            return "$time_passed_m минут назад";
-
-        else:
-            $time_passed_h=$time_passed/3600;
-            return "$time_passed_h часов назад";
-        endif;
-    endif;
+        return "$time_passed_date в $time_passed_m_h";
+    }
+    if($time_passed<3600){
+        $time_passed_m=$time_passed/60;
+        return "$time_passed_m минут назад";
+    }
+    else{
+        $time_passed_h=$time_passed/3600;
+        return "$time_passed_h часов назад";
+    }
 };
 ?>
 
